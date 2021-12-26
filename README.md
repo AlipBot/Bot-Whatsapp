611f8f2fffd81d7f376783a55859547129e4b4c4
Azex229 <muhammad_311959@yes.my>
2021-12-26 13:56:44 +0800

    Bot Whatsapp

---
+++ README.md
@@ -0,0 +1,147 @@
+
+<p align="center">
+<img src="https://avatars.githubusercontent.com/u/80237144?s=400&u=53c85565148ac8a74d4ac6ef9cc379a269c9c507&v=4" width="128" height="128"/>
+</p>
+
+
+<p align="center">
+<a href="#"><img title="Whatsapp-Bot" src="https://img.shields.io/badge/Whatsapp Bot-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+
+
+<p align="center">
+<a href="https://github.com/Azex229"><img title="AUTHOR" src="https://img.shields.io/badge/AUTHOR-AZEX229-yellow.svg?style=for-the-badge&logo=github"></a>
+</p>
+
+<p align="center">
+<a href="https://github.com/ZeeoneOfc"><img title="OwnerScript" src="https://img.shields.io/badge/Owner-ZeeoneOfc-red.svg?style=for-the-badge&logo=github"></a>
+</p>
+
+
+<p align="center">
+<a href="https://www.codefactor.io/repository/github/azex229/bot-whatsapp"><img src="https://www.codefactor.io/repository/github/azex229/bot-whatsapp/badge" alt="CodeFactor" /></a>
+</p>
+
+
+<p align="center">
+<a href="https://github.com/Azex229/followers"><img title="Followers" src="https://img.shields.io/github/followers/azex229?color=blue&style=flat-square"></a>
+<a href="https://github.com/Azex229/Bot-Whatsapp/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/Azex229/Bot-Whatsapp?color=red&style=flat-square"></a>
+<a href="https://github.com/Azex229/Bot-Whatsapp/network/members"><img title="Forks" src="https://img.shields.io/github/forks/Azex229/Bot-Whatsapp?color=red&style=flat-square"></a>
+<a href="https://github.com/Azex229/Bot-Whatsapp/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/Azex229/Bot-Whatsapp?label=Watchers&color=blue&style=flat-square"></a>
+<a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FAzex229%2FBot-Whatsapp&count_bg=%2379C83D&title_bg=%23555555&icon=whatsapp.svg&icon_color=%2329FF00&title=hits&edge_flat=false"/></a>
+</p>
+
+
+## ``Credit 💳 Script``
+script asal [`ZeeoneOfc`](https://github.com/ZeeoneOfc)<br>
+
+------
+
+
+## ``Ubah Script Bot``
+
+
+[`Settings di sini`](https://github.com/Azex229/Bot-Whatsapp/blob/master/settings.json)<br>
+[`file session`](https://github.com/Azex229/Bot-Whatsapp/blob/master/session.json)<br>
+[`tukar gambar`](https://github.com/Azex229/Bot-Whatsapp/tree/master/image)<br>
+[`edit menu`](https://github.com/Azex229/Bot-Whatsapp/blob/master/menu/azex229.js)<br>
+
+
+
+-------
+
+## ``INSTALL IN TERMUX``
+
+```
+termux-setup-storage && apt upgrade && apt update && apt install git && apt install nodejs
+```
+```
+cd /sdcard
+```
+```
+cd (name file bot)
+```
+```
+rm session.json
+```
+```
+npm i && npm start
+```
+``SCAN QR``
+`Kalau qr tidak keluar cuba buang session.json atau update baileys`
+
+------
+
+
+### Create a New Repository
+
+<p align="center">
+<a href="https://github.com/new"><img src="https://i.ibb.co/0y05Lk9/IMG-20211222-090049.jpg" alt="IMG-20211222-090049" border="0"></a><br/>
+</p>
+
+
+## ``UPLOAD FILE TO GITHUB USE TERMUX``
+
+```
+git config --global user.email "isi email github kalian"
+```
+```
+git config --global user.name "isi username github kalian"
+```
+```
+git init
+```
+```
+git add .
+```
+```
+git commit -m "isi apa aja"
+```
+<p align="center">
+<a href="https://github.com/?tab=repositories"><img src="https://i.ibb.co/p0TqLn3/IMG-20211222-210055.jpg" alt="IMG-20211222-210055" border="0"></a>
+</p>
+
+``Copy link link repository``
+
+```
+git remote add origin (isi link yang di copy) 
+```
+```
+git push -u origin master
+```
+
+-----
+
+## ``DEPLOY TO HEROKU``
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/rasssya76/wabot-aq02/tree/patch-1)
+
+CONNECT TO GITHUB
+
+## ``change dyno``
+
+<p align="center">
+<img src="https://i.ibb.co/TMbLDXq/IMG-20211226-125416.jpg" width="650" height="500"/>
+</p> 
+
+## `ADD BUILDPACK for stiker`
+
+<p align="center">
+<img src="https://i.ibb.co/649zBWB/IMG-20211226-125616.jpg" width="650" height="500"/>
+</p> 
+
+```
+https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest
+```
+```
+https://github.com/clhuang/heroku-buildpack-webp-binaries.git
+```
+
+-------
+
+## ```TUTORIAL FULL IN YOUTUBE```
+
+[`dari ZeeoneOfc`](https://youtu.be/5HgB__wARjM)<br>
+
+[`dari gw`](https://youtu.be/5HgB__wARjM)<br>
+
---
+++ menu/azex229.js
@@ -0,0 +1,183 @@
+/*
+   copy and patse Kalau kamu guna termux or replit fitur ini bisa digunakan di Convert Menu
+      
+  ${simbolnya} ${prefix}swm [text|text2]
+  ${simbolnya} ${prefix}take [text|text2]
+
+       tapi di HEROKU gak bisa
+*/
+
+exports.allmenu = (namabot, pushname, sender, isOwner, isPrem, cekvipp, getLimit, limitawal, limit, balance, getBalance, simbolnya, petik, prefix) => {
+return`Hello ${pushname}👋 Saya ${namabot}
+
+📛 Nama : ${pushname} 
+🆔 Nomor : @${sender.split("@")[0]} 
+📊 Status : ${isOwner?`Owner⚔️`:isPrem?`Premium🏅`:`Free User`} 
+🎁 Expired Prem : ${isOwner?`Unlimited Owner`:isPrem ? `${cekvipp.days}d, ${cekvipp.hours}h, ${cekvipp.minutes}m, ${cekvipp.seconds}s`:'Not Premium'}
+💳 Limit : ${isOwner?`Unlimited`: isPrem ? `Unlimited Premium` : getLimit(sender, limitawal, limit) +' / '+limitawal}
+💹 Balance : ${getBalance(sender, balance)}
+
+
+「 *DownloadMenu* 」
+${simbolnya} ${prefix}play [query]
+${simbolnya} ${prefix}ytmp3 [url]
+${simbolnya} ${prefix}ytmp4 [url]
+${simbolnya} ${prefix}video [query]
+${simbolnya} ${prefix}igdl [url]
+${simbolnya} ${prefix}igstory [username]
+${simbolnya} ${prefix}igphoto [url]
+${simbolnya} ${prefix}igreel [url]
+${simbolnya} ${prefix}igvideo [url]
+${simbolnya} ${prefix}igtv [url]
+${simbolnya} ${prefix}tiktok [url]
+${simbolnya} ${prefix}tiktokaudio [url]
+${simbolnya} ${prefix}tiktoknowm [url]
+${simbolnya} ${prefix}mediafire [url]
+
+「  *Convert Menu* 」
+${simbolnya} ${prefix}sticker [tag foto/vid]
+${simbolnya} ${prefix}attp [text]
+${simbolnya} ${prefix}semoji 🐦
+${simbolnya} ${prefix}toimg [reply stiker]
+${simbolnya} ${prefix}tourl [reply foto/vid]
+${simbolnya} ${prefix}tomp4 [reply stiker gif]
+${simbolnya} ${prefix}togif [reply stiker gif]
+${simbolnya} ${prefix}tomp3 [reply vid]
+${simbolnya} ${prefix}tovn [reply music]
+${simbolnya} ${prefix}tinyurl [link]
+${simbolnya} ${prefix}kontak 62***|nama
+${simbolnya} ${prefix}bass  [reply music]
+${simbolnya} ${prefix}slow  [reply music]
+${simbolnya} ${prefix}fast [reply music]
+${simbolnya} ${prefix}robot [reply music]
+${simbolnya} ${prefix}detikvn [durasi]
+${simbolnya} ${prefix}tupai [reply music]
+${simbolnya} ${prefix}vibrato [reply music]
+
+「  *Editor Menu* 」
+${simbolnya} ${prefix}memegen teks atas | teks bawah
+${simbolnya} ${prefix}stickmeme teks
+
+「  *Storage Menu* 」
+${simbolnya} ${prefix}addrespon [pesan|respon]
+${simbolnya} ${prefix}delrespon [pesan]
+${simbolnya} ${prefix}listrespon
+${simbolnya} ${prefix}listbadword
+${simbolnya} ${prefix}addbadword [text]
+${simbolnya} ${prefix}delbadword [text]
+${simbolnya} ${prefix}addvn [text]
+${simbolnya} ${prefix}listvn
+${simbolnya} ${prefix}delvn [text]
+${simbolnya} ${prefix}addimage [text]
+${simbolnya} ${prefix}listimage
+${simbolnya} ${prefix}delimage [text]
+${simbolnya} ${prefix}addvideo [text]
+${simbolnya} ${prefix}listvideo 
+${simbolnya} ${prefix}delvideo [text]
+${simbolnya} ${prefix}addsticker [text]
+${simbolnya} ${prefix}liststicker
+${simbolnya} ${prefix}delstik [text]
+
+「  *Fun Menu* 」
+${simbolnya} ${prefix}slot
+${simbolnya} ${prefix}truth
+${simbolnya} ${prefix}dare
+${simbolnya} ${prefix}voting @tag | alasan | waktu
+${simbolnya} ${prefix}delvote
+   vote
+   devote
+
+「  *Group Menu* 」
+${simbolnya} ${prefix}sider [reply pesan bot]
+${simbolnya} ${prefix}afk [reason]
+${simbolnya} ${prefix}hidetag [text]
+${simbolnya} ${prefix}notif [text]
+${simbolnya} ${prefix}jadian
+${simbolnya} ${prefix}listonline
+${simbolnya} ${prefix}tagall
+${simbolnya} ${prefix}ceksewa
+${simbolnya} ${prefix}add 628XX
+${simbolnya} ${prefix}kick @tag
+${simbolnya} ${prefix}promote @tag
+${simbolnya} ${prefix}demote @tag
+${simbolnya} ${prefix}resetlink
+${simbolnya} ${prefix}linkgc
+${simbolnya} ${prefix}setdesc [text]
+${simbolnya} ${prefix}setnamagrup [text]
+${simbolnya} ${prefix}grup [open/close]
+${simbolnya} ${prefix}antibadword [on/off]
+${simbolnya} ${prefix}welcome [on/off]
+${simbolnya} ${prefix}antilink [on/off]
+${simbolnya} ${prefix}antitroli [on/off]
+
+「  *Limit Menu* 」
+${simbolnya} ${prefix}topbalance
+${simbolnya} ${prefix}toplimit
+${simbolnya} ${prefix}limit
+${simbolnya} ${prefix}level
+${simbolnya} ${prefix}balance
+${simbolnya} ${prefix}buylimit [nominal]
+${simbolnya} ${prefix}giftlimit @tag | nominal
+${simbolnya} ${prefix}buyprem [need 1k limit]
+   
+「  *Maker Menu* 」
+${simbolnya} ${prefix}nuliskanan [text]
+${simbolnya} ${prefix}nuliskiri [text]
+${simbolnya} ${prefix}foliokanan [text]
+${simbolnya} ${prefix}foliokiri [text]
+${simbolnya} ${prefix}naruto [text]
+${simbolnya} ${prefix}shadow [text]
+${simbolnya} ${prefix}lovemsg [text]
+${simbolnya} ${prefix}doubleheart [text]
+${simbolnya} ${prefix}coffecup [text]
+${simbolnya} ${prefix}lovetext [text]
+${simbolnya} ${prefix}butterfly [text]
+${simbolnya} ${prefix}romantic [text]
+${simbolnya} ${prefix}smoke [text]
+${simbolnya} ${prefix}burnpaper [text]
+${simbolnya} ${prefix}grass [text]
+ 
+「  *Search Menu* 」
+${simbolnya} ${prefix}pinterest [query]
+${simbolnya} ${prefix}wikipedia [query]
+${simbolnya} ${prefix}playstore [query]
+${simbolnya} ${prefix}yts [query]
+
+ _guna fitur ini dengan sebaik_  
+
+「  *Others Menu* 」
+${simbolnya} ${prefix}pantun
+${simbolnya} ${prefix}faktaunik
+${simbolnya} ${prefix}donasi
+${simbolnya} ${prefix}nulis
+${simbolnya} ${prefix}getnumber [reply user]
+${simbolnya} ${prefix}getpic @tag
+${simbolnya} ${prefix}wame 
+${simbolnya} ${prefix}towame
+
+「  *Owner Menu* 」
+${simbolnya} ${prefix}bc [text]
+${simbolnya} ${prefix}bcimage [reply foto]
+${simbolnya} ${prefix}ban @tag
+${simbolnya} ${prefix}unban @tag
+${simbolnya} ${prefix}block @tag
+${simbolnya} ${prefix}unblock @tag
+${simbolnya} ${prefix}setbiobot [text]
+${simbolnya} ${prefix}setnamabot [text]
+${simbolnya} ${prefix}resetlimit
+${simbolnya} ${prefix}clearall
+${simbolnya} ${prefix}prem add @tag waktu
+${simbolnya} ${prefix}prem del
+${simbolnya} ${prefix}sewa add waktu
+${simbolnya} ${prefix}sewa del
+${simbolnya} ${prefix}setcmd [reply stiker]
+${simbolnya} ${prefix}delcmd [reply stiker]
+${simbolnya} ${prefix}listcmd
+${simbolnya} ${prefix}auto [ketik/vn]
+${simbolnya}  > [eval]
+${simbolnya}  => [async]
+${simbolnya}  $ [cmd termux]
+
+ 「 *BotWhatsaap* 」
+`
+}
